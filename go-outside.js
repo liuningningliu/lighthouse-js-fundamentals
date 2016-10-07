0d8fcbbbb9c01188a4a9e3cfdf0c1b95be0a63db
ning <ningliu911@gmail.com>
2016-10-07 21:12:45 +0000

    Adds conditionals ercrcises

---
+++ go-outside.js
@@ -0,0 +1,12 @@
+
+var temperature = 12;
+
+if (temperature < 0) {
+  conosole.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  conosole.log("Short sleeves won't cut it!");
+} else {
+  conosole.log("Short sleeves are fine.");
+}
+
+conosole.log("Now you're ready to go outside!");
